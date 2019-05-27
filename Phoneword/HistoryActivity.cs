d7eb26f39e990d9d6320be2b756ed91409bbe9bc
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2019-05-27 12:10:37 +0300

    Добавьте файлы проекта.

---
+++ Phoneword/HistoryActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Phoneword
+{
+    [Activity(Label = "@string/translation_history", Theme = "@style/AppTheme", NoHistory = false)]
+    class HistoryActivity: ListActivity
+    {
+
+        string []items;
+
+        public const string PHONE_WORD_KEY = "phone_word";
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+           
+            items = Intent.GetStringArrayExtra(MainActivity.BUNDLE_HISTORY_KEY);
+            this.ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, items);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            base.OnListItemClick(l, v, position, id);
+            Intent intent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.PutString(PHONE_WORD_KEY, items[position]);
+            intent.PutExtras(bundle);
+            this.SetResult(Result.Ok, intent);
+            Finish();
+        }
+    }
+}
---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,108 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Views;
+using System.Collections.Generic;
+
+namespace Phoneword
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, NoHistory = false)]
+    public class MainActivity : AppCompatActivity
+    {
+        public const string BUNDLE_HISTORY_KEY = "history";
+        private const int HISTORY_REQ_CODE = 202;
+
+        EditText phoneNumberText;
+        TextView translatedPhoneWord;
+        List<string> history = new List<string>();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                if (!string.IsNullOrWhiteSpace(phoneNumberText.Text))
+                {
+                    history.Add(phoneNumberText.Text);
+                    string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                    if (string.IsNullOrWhiteSpace(translatedNumber))
+                    {
+                        translatedPhoneWord.Text = string.Empty;
+                    }
+                    else
+                    {
+                        translatedPhoneWord.Text = translatedNumber;
+                    }
+                }
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            this.MenuInflater.Inflate(Resource.Menu.main_menu, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.about_item:
+                    return true;
+                case Resource.Id.history_item:
+                    Bundle bundle = new Bundle();
+                    bundle.PutStringArray(BUNDLE_HISTORY_KEY, history.ToArray());
+                    var intent = new Intent(this, typeof(HistoryActivity));
+                    intent.PutExtras(bundle);
+                    StartActivityForResult(intent, HISTORY_REQ_CODE);
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode != HISTORY_REQ_CODE || resultCode != Result.Ok)
+                return;
+            if (data == null)
+                return;
+
+            var phoneWord = data.GetStringExtra(HistoryActivity.PHONE_WORD_KEY);
+            if (string.IsNullOrWhiteSpace(phoneWord))
+            {
+                phoneNumberText.Text = string.Empty;
+                translatedPhoneWord.Text = string.Empty;
+                return;
+            }
+
+            phoneNumberText.Text = phoneWord;
+            string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneWord);
+            if (string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                translatedPhoneWord.Text = string.Empty;
+            }
+            else
+            {
+                translatedPhoneWord.Text = translatedNumber;
+            }
+
+        }
+    }
+}
