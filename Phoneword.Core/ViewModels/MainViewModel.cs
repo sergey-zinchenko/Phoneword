293040a19cab411fe58b9ce592c175948b464b55
Korota Alex <norther48@list.ru>
2019-05-28 11:40:11 +0300

    add core project with only main viewmodel

---
+++ Phoneword.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using MvvmCross.ViewModels;
+using Phoneword.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoneword.Core.ViewModels
+{
+    class MainViewModel : MvxViewModel
+    {
+        readonly ITranslationService _translationService;
+
+        public MainViewModel(ITranslationService translationService)
+        {
+            _translationService = translationService;
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+
+            _phoneNumberText = "adgjmptw";
+            Translate();
+        }
+
+        private string _phoneNumberText;
+        public string PhoneNumberText
+        {
+            get => _phoneNumberText;
+            set
+            {
+                _phoneNumberText = value;
+                RaisePropertyChanged(() => PhoneNumberText);
+            }
+        }
+
+        private string _translatedPhoneword;
+        public string TranslatedPhoneword
+        {
+            get => _translatedPhoneword;
+            set
+            {
+                _translatedPhoneword = value;
+                RaisePropertyChanged(() => TranslatedPhoneword);
+            }
+        }
+
+        private void Translate()
+        {
+            TranslatedPhoneword = _translationService.ToNumber(_phoneNumberText);
+        }
+    }
+}
