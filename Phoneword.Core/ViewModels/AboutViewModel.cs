befc0f3bbf4c2d1b048a24e3e62b9094fb183e18
Korota Alex <norther48@list.ru>
2019-05-29 16:49:55 +0300

    about activity done

--- Phoneword.Core/ViewModels/AboutViewModel.cs
+++ Phoneword.Core/ViewModels/AboutViewModel.cs
@@ -1,11 +1,28 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Phoneword.Core.ViewModels
 {
     public class AboutViewModel : MvxViewModel
     {
+        private readonly IMvxNavigationService _navigationService;
+        public IMvxCommand OkClickCommand { get; private set; }
+
+        public AboutViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            OkClickCommand = new MvxAsyncCommand(async () => await _navigationService.Close(this));
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+        }
     }
 }
