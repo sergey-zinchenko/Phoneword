3d72e3da6e419f596250b930dfaa9bc08339f782
Korota Alex <norther48@list.ru>
2019-05-29 18:01:17 +0300

    need to add  chhosen string

--- Phoneword.Droid/Views/HistoryView.cs
+++ Phoneword.Droid/Views/HistoryView.cs
@@ -9,10 +9,18 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using Phoneword.Core.ViewModels;
 
 namespace Phoneword.Droid.Views
 {
-    public class HistoryView
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class HistoryView : MvxAppCompatActivity<HistoryViewModel>
     {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_history);
+        }
     }
 }
