033e8709ab8ae63f1bbc75007d6ee296163372e3
Korota Alex <norther48@list.ru>
2019-05-29 17:26:28 +0300

    way to historyview

--- Phoneword.Core/ViewModels/HistoryViewModel.cs
+++ Phoneword.Core/ViewModels/HistoryViewModel.cs
@@ -2,11 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Phoneword.Core.ViewModels
 {
-    public class HistoryViewModel : MvxViewModel
+    public class HistoryViewModel : MvxViewModel<List<string>, string>
     {
+        List<string> history;
+        public override void Prepare(List<string> parameter)
+        {
+            history = parameter;
+        }
 
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+        }
     }
 }
