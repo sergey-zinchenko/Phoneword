23c565ed6b42cba61906ce2352669552c597084c
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2019-05-30 15:20:43 +0300

    ios project added

---
+++ Phoneword.iOS/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using UIKit;
+using Phoneword.Core;
+using MvvmCross.Platforms.Ios.Core;
+
+namespace Phoneword.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+        public override UIWindow Window { get; set; }
+
+        // FinishedLaunching is the very first code to be executed in your app. Don't forget to call base!
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            var result = base.FinishedLaunching(application, launchOptions);
+
+            return result;
+        }
+    }
+}
