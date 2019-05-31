fb07a7dcc703081f55538c7df85f6600227625da
sergey <sergey.zinchenko.rnd@gmail.com>
2019-05-31 16:28:14 +0300

    test ios view added

---
+++ Phoneword.iOS/Views/MainView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Phoneword.iOS.Views
+{
+    public partial class MainView : UIViewController
+    {
+        public MainView() : base("MainView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
