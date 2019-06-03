261c2d757d0b89bbdc6fc3474455ef94a5217e71
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2019-06-03 20:17:18 +0300

    AboutView xamarin forms mvvmcross

---
+++ Phoneword.Forms.UI/Pages/AboutView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.Forms.Views;
+using Phoneword.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Phoneword.Forms.UI.Pages
+{
+   // [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutView : MvxContentPage<AboutViewModel>
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
