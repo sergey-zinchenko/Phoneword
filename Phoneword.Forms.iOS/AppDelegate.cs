3cd408e7fc06641e5b40bc660ec374d7171f073d
Korota Alex <norther48@list.ru>
2019-06-04 13:59:38 +0300

    add iosFormsProject

---
+++ Phoneword.Forms.iOS/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using Phoneword.Core;
+using Phoneword.Forms.UI;
+using UIKit;
+
+namespace Phoneword.Forms.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<App, FormsApp>, App, FormsApp>
+    {
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+        {
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+    }
+}
+
+
