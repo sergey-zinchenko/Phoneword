09370e78fa56b774fa7f20902daa0959ea0e41ea
Korota Alex <norther48@list.ru>
2019-06-04 12:32:33 +0300

    add history view

---
+++ Phoneword.Forms.UI/Pages/HistoryView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MvvmCross.Forms.Views;
+using Phoneword.Core.ViewModels;
+
+
+namespace Phoneword.Forms.UI.Pages
+{
+    public partial class HistoryView : MvxContentPage<HistoryViewModel>
+    {
+        public HistoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
