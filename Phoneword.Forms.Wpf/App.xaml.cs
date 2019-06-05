6431eb3ca3363e9959ab1406c5170bc0020bf3f0
Korota Alex <norther48@list.ru>
2019-06-05 10:55:18 +0300

    wpf project added

---
+++ Phoneword.Forms.Wpf/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Forms.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+using Phoneword.Forms.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Phoneword.Forms.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : MvxApplication
+    {
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<MvxFormsWpfSetup<Phoneword.Core.App, FormsApp>>();
+        }
+    }
+}
